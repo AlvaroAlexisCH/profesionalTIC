0bd4585b05af5eafab9e2ae223b956120ccde22d
Alexis <U22225259@utp.edu.pe>
2026-08-22 14:30:27 -0500

    segundo

--- tests/logic.test.js
+++ tests/logic.test.js
@@ -1,8 +1,9 @@
-const { sumar, restar, multiplicar, dividir, calcularOperacion } = require("../logic");
+const { sumar, restar, multiplicar, dividir, potencia, calcularOperacion } = require("../logic");
 
 test("2 + 3 = 5", () => expect(sumar(2, 3)).toBe(5));
 test("10 - 4 = 6", () => expect(restar(10, 4)).toBe(6));
 test("3 * 4 = 12", () => expect(multiplicar(3, 4)).toBe(12));
 test("10 / 2 = 5", () => expect(dividir(10, 2)).toBe(5));
 test("dividir entre cero genera error", () => expect(() => dividir(10, 0)).toThrow());
+test("2 ** 2 = 4", () => expect(potencia(2)).toBe(4));
 test("calcularOperacion ejecuta suma", () => expect(calcularOperacion(7, 8, "sumar")).toBe(15));
